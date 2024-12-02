475e8bca1a285d27e104d603a79257c5e818990b
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-02 11:15:56 +0530

    Added GetNewsSongsUseCase & Updated initializeDependencies with latest dependencies

---
+++ lib/domain/usecases/song/get_news_songs.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:spotify_clone/core/usecase/usecase.dart';
+import 'package:spotify_clone/data/models/failure.dart';
+import 'package:spotify_clone/domain/entities/song/song.dart';
+import 'package:spotify_clone/domain/repository/song/song_repository.dart';
+
+import '../../../service_locator.dart';
+
+class GetNewsSongsUseCase
+    implements UseCase<Either<Failure, List<SongEntity>>, dynamic> {
+  @override
+  Future<Either<Failure, List<SongEntity>>> call({param}) async {
+    return sl<SongRepository>().getNewsSongs();
+  }
+}
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,10 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify_clone/data/repository/auth/auth_repository_impl.dart';
+import 'package:spotify_clone/data/repository/song/song_repository_impl.dart';
 import 'package:spotify_clone/data/sources/auth/auth_service.dart';
+import 'package:spotify_clone/data/sources/song/song_service.dart';
 import 'package:spotify_clone/domain/repository/auth/auth.dart';
+import 'package:spotify_clone/domain/repository/song/song_repository.dart';
 
 import 'domain/usecases/auth/signin.dart';
 import 'domain/usecases/auth/signup.dart';
+import 'domain/usecases/song/get_news_songs.dart';
 
 final sl = GetIt.instance;
 
@@ -13,4 +17,7 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
   sl.registerSingleton<SignInUseCase>(SignInUseCase());
   sl.registerSingleton<SignUpUseCase>(SignUpUseCase());
+  sl.registerSingleton<SongService>(SongFirebaseService());
+  sl.registerSingleton<SongRepository>(SongRepositoryImpl());
+  sl.registerSingleton<GetNewsSongsUseCase>(GetNewsSongsUseCase());
 }
