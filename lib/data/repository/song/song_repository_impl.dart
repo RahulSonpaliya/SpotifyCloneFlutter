cb51a8099dae671cc089bcf6e7da34e1a2f8086f
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-02 19:12:48 +0530

    implement addOrRemoveFavSong, isFavSong & getUsersFavoriteSongs methods in SongRepository and SongRepositoryImpl

--- lib/data/repository/song/song_repository_impl.dart
+++ lib/data/repository/song/song_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:spotify_clone/data/models/base_response.dart';
 import 'package:spotify_clone/data/models/failure.dart';
 import 'package:spotify_clone/data/sources/song/song_service.dart';
 import 'package:spotify_clone/domain/entities/song/song.dart';
@@ -15,5 +16,21 @@ class SongRepositoryImpl extends SongRepository {
   @override
   Future<Either<Failure, List<SongEntity>>> getPlaylist() async {
     return sl<SongService>().getPlaylist();
+  }
+
+  @override
+  Future<Either<Failure, BaseResponse>> addOrRemoveFavSong(
+      String songId) async {
+    return sl<SongService>().addOrRemoveFavSong(songId);
+  }
+
+  @override
+  Future<Either<Failure, List<SongEntity>>> getUsersFavoriteSongs() async {
+    return sl<SongService>().getUsersFavoriteSongs();
+  }
+
+  @override
+  Future<Either<Failure, BaseResponse>> isFavSong(String songId) async {
+    return sl<SongService>().isFavSong(songId);
   }
 }
--- lib/domain/repository/song/song_repository.dart
+++ lib/domain/repository/song/song_repository.dart
@@ -2,8 +2,16 @@ import 'package:dartz/dartz.dart';
 import 'package:spotify_clone/data/models/failure.dart';
 import 'package:spotify_clone/domain/entities/song/song.dart';
 
+import '../../../data/models/base_response.dart';
+
 abstract class SongRepository {
   Future<Either<Failure, List<SongEntity>>> getNewsSongs();
 
   Future<Either<Failure, List<SongEntity>>> getPlaylist();
+
+  Future<Either<Failure, BaseResponse>> addOrRemoveFavSong(String songId);
+
+  Future<Either<Failure, BaseResponse>> isFavSong(String songId);
+
+  Future<Either<Failure, List<SongEntity>>> getUsersFavoriteSongs();
 }
