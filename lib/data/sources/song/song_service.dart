52aafbf54b51d9368c0f63e9dab2cf394df7c8ca
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-02 18:54:36 +0530

    Added addOrRemoveFavSong to save/delete user's favorites song from firestore

--- lib/data/sources/song/song_service.dart
+++ lib/data/sources/song/song_service.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify_clone/data/models/base_response.dart';
 import 'package:spotify_clone/data/models/song/song.dart';
 
 import '../../../domain/entities/song/song.dart';
@@ -9,6 +11,8 @@ abstract class SongService {
   Future<Either<Failure, List<SongEntity>>> getNewsSongs();
 
   Future<Either<Failure, List<SongEntity>>> getPlaylist();
+
+  Future<Either<Failure, BaseResponse>> addOrRemoveFavSong(String songId);
 }
 
 class SongFirebaseService extends SongService {
@@ -40,6 +44,38 @@ class SongFirebaseService extends SongService {
         songs.add(songModel.toEntity());
       }
       return Right(songs);
+    } catch (e) {
+      return Left(Failure(0, "Some error occurred, please try again."));
+    }
+  }
+
+  @override
+  Future<Either<Failure, BaseResponse>> addOrRemoveFavSong(
+      String songId) async {
+    try {
+      final firebaseAuth = FirebaseAuth.instance;
+      final firebaseFirestore = FirebaseFirestore.instance;
+      final user = firebaseAuth.currentUser;
+      final uid = user!.uid;
+      var responseMessage = '';
+      final favoriteSongs = await firebaseFirestore
+          .collection('Users')
+          .doc(uid)
+          .collection('Favorites')
+          .where('songId', isEqualTo: songId)
+          .get();
+      if (favoriteSongs.docs.isNotEmpty) {
+        await favoriteSongs.docs.first.reference.delete();
+        responseMessage = 'Favorite removed successfully';
+      } else {
+        await firebaseFirestore
+            .collection('Users')
+            .doc(uid)
+            .collection('Favorites')
+            .add({'songId': songId, 'addedDate': Timestamp.now()});
+        responseMessage = 'Favorite added successfully';
+      }
+      return Right(BaseResponse(message: responseMessage));
     } catch (e) {
       return Left(Failure(0, "Some error occurred, please try again."));
     }
