91d6a771f12b447a34d11f7fa0fdeb4923b60c5e
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-04 16:38:06 +0530

    calling UploadProfileImageUseCase to upload users-profile-image after picking it up from gallery or camera

--- lib/presentation/profile/bloc/profile_info_cubit.dart
+++ lib/presentation/profile/bloc/profile_info_cubit.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:spotify_clone/domain/entities/auth/user.dart';
 import 'package:spotify_clone/domain/usecases/auth/get_user.dart';
+import 'package:spotify_clone/domain/usecases/auth/upload_profile_image.dart';
 import 'package:spotify_clone/presentation/profile/bloc/profile_info_state.dart';
 
 import '../../../service_locator.dart';
@@ -23,11 +24,17 @@ class ProfileInfoCubit extends Cubit<ProfileInfoState> {
   late UserEntity userEntity;
   XFile? updatedUserImage;
 
-  void userImagePicked(XFile updatedUserImage) {
+  void userImagePicked(XFile updatedUserImage) async {
     this.updatedUserImage = updatedUserImage;
     emit(ProfileInfoLoaded(
       userEntity: userEntity,
       updatedUserImage: updatedUserImage,
     ));
+    var result = await sl<UploadProfileImageUseCase>()
+        .call(param: updatedUserImage.path);
+    result.fold(
+      (failure) {},
+      (response) => getUser(),
+    );
   }
 }
