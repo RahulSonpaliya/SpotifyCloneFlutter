232dc5b384da7bdca65056bcce795f13c2d3c7b5
RahulSonpaliya <rahulsonpaliya@gmail.com>
2024-12-04 18:13:43 +0530

    Implements loader overlay on auth pages.

--- lib/presentation/auth/pages/login.dart
+++ lib/presentation/auth/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:spotify_clone/common/widgets/app_bar/basic_app_bar.dart';
 import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/data/models/auth/signin_user_req.dart';
@@ -16,101 +17,105 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const BasicAppBar(
-        showLogo: true,
-        showBackButton: true,
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Not A Member?',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => SignUpPage(),
-                  ),
-                );
-              },
-              child: const Text(
-                'Register Now',
-              ),
-            )
-          ],
+    return LoaderOverlay(
+      child: Scaffold(
+        appBar: const BasicAppBar(
+          showLogo: true,
+          showBackButton: true,
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(
-          vertical: 40,
-          horizontal: 30,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Not A Member?',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => SignUpPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Register Now',
+                ),
+              )
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            const Text(
-              'Sign In',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            vertical: 40,
+            horizontal: 30,
+          ),
+          child: Column(
+            children: [
+              const Text(
+                'Sign In',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            TextField(
-              controller: _emailCtrl,
-              decoration: const InputDecoration(
-                hintText: 'Enter Email',
+              const SizedBox(
+                height: 50,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _passwordCtrl,
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Password',
+              TextField(
+                controller: _emailCtrl,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Email',
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            BasicAppButton(
-              onPressed: () async {
-                var result = await sl<SignInUseCase>().call(
-                    param: SignInUserReq(
-                  email: _emailCtrl.text,
-                  password: _passwordCtrl.text,
-                ));
-                result.fold(
-                  (failure) {
-                    var snackBar = SnackBar(
-                      content: Text(failure.message),
-                      behavior: SnackBarBehavior.floating,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  (signInResponse) {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (_) => const HomePage()),
-                        (route) => false);
-                  },
-                );
-              },
-              title: 'Sign In',
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _passwordCtrl,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              BasicAppButton(
+                onPressed: () async {
+                  context.loaderOverlay.show();
+                  var result = await sl<SignInUseCase>().call(
+                      param: SignInUserReq(
+                    email: _emailCtrl.text,
+                    password: _passwordCtrl.text,
+                  ));
+                  context.loaderOverlay.hide();
+                  result.fold(
+                    (failure) {
+                      var snackBar = SnackBar(
+                        content: Text(failure.message),
+                        behavior: SnackBarBehavior.floating,
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    (signInResponse) {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (_) => const HomePage()),
+                          (route) => false);
+                    },
+                  );
+                },
+                title: 'Sign In',
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:spotify_clone/common/widgets/app_bar/basic_app_bar.dart';
 import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/data/models/auth/create_user_req.dart';
@@ -17,111 +18,115 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const BasicAppBar(
-        showLogo: true,
-        showBackButton: true,
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Do you have an account?',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                fontSize: 14,
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => LoginPage(),
-                  ),
-                );
-              },
-              child: const Text(
-                'Sign In',
-              ),
-            )
-          ],
+    return LoaderOverlay(
+      child: Scaffold(
+        appBar: const BasicAppBar(
+          showLogo: true,
+          showBackButton: true,
         ),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(
-          vertical: 40,
-          horizontal: 30,
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Do you have an account?',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => LoginPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Sign In',
+                ),
+              )
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            const Text(
-              'Register',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            vertical: 40,
+            horizontal: 30,
+          ),
+          child: Column(
+            children: [
+              const Text(
+                'Register',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            TextField(
-              controller: _fullNameCtrl,
-              decoration: const InputDecoration(
-                hintText: 'Full Name',
+              const SizedBox(
+                height: 50,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _emailCtrl,
-              decoration: const InputDecoration(
-                hintText: 'Enter Email',
+              TextField(
+                controller: _fullNameCtrl,
+                decoration: const InputDecoration(
+                  hintText: 'Full Name',
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _passwordCtrl,
-              obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Password',
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            BasicAppButton(
-              onPressed: () async {
-                var result = await sl<SignUpUseCase>().call(
-                    param: CreateUserReq(
-                  fullName: _fullNameCtrl.text,
-                  email: _emailCtrl.text,
-                  password: _passwordCtrl.text,
-                ));
-                result.fold(
-                  (failure) {
-                    var snackBar = SnackBar(
-                      content: Text(failure.message),
-                      behavior: SnackBarBehavior.floating,
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  },
-                  (signUpResponse) {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (_) => const HomePage()),
-                        (route) => false);
-                  },
-                );
-              },
-              title: 'Create Account',
-            )
-          ],
+              TextField(
+                controller: _emailCtrl,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Email',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _passwordCtrl,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              BasicAppButton(
+                onPressed: () async {
+                  context.loaderOverlay.show();
+                  var result = await sl<SignUpUseCase>().call(
+                      param: CreateUserReq(
+                    fullName: _fullNameCtrl.text,
+                    email: _emailCtrl.text,
+                    password: _passwordCtrl.text,
+                  ));
+                  context.loaderOverlay.hide();
+                  result.fold(
+                    (failure) {
+                      var snackBar = SnackBar(
+                        content: Text(failure.message),
+                        behavior: SnackBarBehavior.floating,
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    (signUpResponse) {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (_) => const HomePage()),
+                          (route) => false);
+                    },
+                  );
+                },
+                title: 'Create Account',
+              )
+            ],
+          ),
         ),
       ),
     );
